b539994d3a2f83f21198bc02be19db26f30d2b16
Mariano Da Silva Lemos <lemosmariano336@gmail.com>
2026-03-10 20:46:52 -0300

    Update database.py

--- CRUD/database.py
+++ CRUD/database.py
@@ -19,7 +19,7 @@ class transactionObject():
 
     def execute(self, sql, parms = None):
         if transactionObject.connected:
-            if parms None:
+            if parms == None:
                 transactionObject.cur.execute(sql)
             else:
                 transactionObject.cur.execute(sql, parms)
